a74c5242af6c5bce0f1189839f7f2d20701b9300
drexler <andrew.quartey@gmail.com>
2017-08-23 12:59:13 -0400

    initial commit

---
+++ Velson/src/com/drexler/JsonParser.java
@@ -0,0 +1,68 @@
+package com.drexler;
+
+import java.util.*;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class JsonParser {
+
+    private List<String> pathList;
+    private String json;
+
+    public JsonParser(String json) {
+        this.json = json;
+        this.pathList = new ArrayList<String>();
+        setJsonPaths(json);
+    }
+
+    public List<String> getPathList() {
+        return this.pathList;
+    }
+
+    private void setJsonPaths(String json) {
+        this.pathList = new ArrayList<String>();
+        JSONObject object = new JSONObject(json);
+        String jsonPath = "$";
+        if(json != JSONObject.NULL) {
+            readObject(object, jsonPath);
+        }   
+    }
+
+    private void readObject(JSONObject object, String jsonPath) {
+        Iterator<String> keysItr = object.keys();
+        String parentPath = jsonPath;
+        while(keysItr.hasNext()) {
+            String key = keysItr.next();
+            Object value = object.get(key);
+            jsonPath = parentPath + "." + key;
+
+            if(value instanceof JSONArray) {            
+                readArray((JSONArray) value, jsonPath);
+            }
+            else if(value instanceof JSONObject) {
+                readObject((JSONObject) value, jsonPath);
+            } else { // is a value
+                this.pathList.add(jsonPath);    
+            }          
+        }  
+    }
+
+    private void readArray(JSONArray array, String jsonPath) {      
+        String parentPath = jsonPath;
+        for(int i = 0; i < array.length(); i++) {
+            Object value = array.get(i);        
+            jsonPath = parentPath + "[" + i + "]";
+
+            if(value instanceof JSONArray) {
+                readArray((JSONArray) value, jsonPath);
+            } else if(value instanceof JSONObject) {                
+                readObject((JSONObject) value, jsonPath);
+            } else { // is a value
+                this.pathList.add(jsonPath);
+            }       
+        }
+    }
+
+}
+
