d673e6ce13ae298dc0b133126bc473c77c7e1144
drexler <andrew.quartey@gmail.com>
2017-09-26 23:06:56 -0400

    add tests for velson engine

---
+++ src/test/java/com/drexler/velson/VelsonEngineTest.java
@@ -0,0 +1,67 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import org.json.JSONObject;
+
+import com.drexler.velson.VelsonEngine;
+import com.drexler.velson.ResourceLocale;
+
+
+public class VelsonEngineTest
+{
+   private String template = "template.vm";
+   private String jsonFile = "test.json";
+
+   @Test
+   public void givenNoLocaleEngineShouldDefaultToClasspathResources()
+   {
+      VelsonEngine velsonEngine = new VelsonEngine(template, jsonFile);
+
+      try
+      {
+         JSONObject output = velsonEngine.transform();
+         assertTrue(true);
+      }
+      catch (Exception ex)
+      {
+         fail("Exception shouldn't be thrown!");
+      }
+   }
+
+   @Test
+   public void givenClasspathLocaleEngineShouldUseClasspathResources()
+   {
+      VelsonEngine velsonEngine = new VelsonEngine(ResourceLocale.classpath, template, jsonFile);
+
+      try
+      {
+         JSONObject output = velsonEngine.transform();
+         assertTrue(true);
+      }
+      catch (Exception ex)
+      {
+         fail("Exception shouldn't be thrown!");
+      }
+   }
+
+   @Test
+   public void givenFileSystemLocaleEngineShouldUseSpecifiedFiles()
+   {
+      String testClassPath = VelsonEngineTest.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+      String testResourceFolderPath = testClassPath + "../../resources/test/";
+      String templatePath = testResourceFolderPath + template;
+      String jsonFilePath = testResourceFolderPath + jsonFile;
+
+      VelsonEngine velsonEngine = new VelsonEngine(ResourceLocale.fileSystem, templatePath, jsonFilePath);
+
+      try
+      {
+         JSONObject output = velsonEngine.transform();
+         assertTrue(true);
+      }
+      catch (Exception ex)
+      {
+         fail("Exception shouldn't be thrown!");
+      }
+   }
+}
