6305d7bc96230c5ec1f58e8eed96cb7c99d3e06e
drexler <andrew.quartey@gmail.com>
2017-10-22 21:47:34 -0400

    add encryped gradle.properties file

---
+++ travis-build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+echo "TRAVIS_TAG          : $TRAVIS_TAG"
+echo "TRAVIS_BRANCH       : $TRAVIS_BRANCH"
+echo "TRAVIS_PULL_REQUEST : $TRAVIS_PULL_REQUEST"
+echo "Publishing archives for branch $TRAVIS_BRANCH"
+
+echo "Cleaning..."
+./gradlew clean check install --stacktrace
+
+echo "Building"
+./gradlew build
+
+echo "Tests"
+./gradlew test
